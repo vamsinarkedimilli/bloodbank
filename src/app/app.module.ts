fc450a730913976af938a975d9f43a1f6af224b8
Krishna Vamsi <77627471+vamsi-9597@users.noreply.github.com>
2022-02-26 14:48:28 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DonateComponent } from './donate/donate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule
